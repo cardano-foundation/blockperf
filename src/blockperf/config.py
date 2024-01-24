2228c5002981ed2de978802add696f8319ad202e
Manuel Schmidt <manuel.schmidt@cardanofoundation.org>
2024-01-24 13:46:54 +0000

    Fix TracingVerbosity config check

--- src/blockperf/config.py
+++ src/blockperf/config.py
@@ -101,10 +101,10 @@ class AppConfig:
             "TraceBlockFetchClient", False
         ), "TraceBlockFetchClient not enabled"
         # What are the other possible values? This should allow everything that is above Normal
-        assert (
-            self.node_config.get("TracingVerbosity", "")
-            in ("NormalVerbosity", "MaximalVerbosity"),
-        ), "TracingVerbosity not enabled"
+        assert self.node_config.get("TracingVerbosity", "") in (
+            "NormalVerbosity",
+            "MaximalVerbosity",
+        ), "TracingVerbosity must be NormalVerbosity or MaximalVerbosity"
 
     @property
     def broker_host(self) -> str:
