1142eaa767bb649afb9996a3b4aff05c290284ae
Manuel Schmidt <manuel.schmidt@cardanofoundation.org>
2024-01-24 13:13:01 +0000

    Remove python version print

--- src/blockperf/cli.py
+++ src/blockperf/cli.py
@@ -90,8 +90,6 @@ def main():
     if already_running():
         sys.exit("Blockperf is already running")
 
-    sys.stdout.write(f"Python: {sys.version}")
-
     app_config = AppConfig()
     app = App(app_config)
 
